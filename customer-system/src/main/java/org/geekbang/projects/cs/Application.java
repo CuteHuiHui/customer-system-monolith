167d37ca8f0f5b625916fbb2fc058539e490ae9a
huixia.hu <huixia.hu@dbappsecurity.com.cn>
2023-08-14 15:33:09 +0800

    feign接口调用

--- customer-system/src/main/java/org/geekbang/projects/cs/Application.java
+++ customer-system/src/main/java/org/geekbang/projects/cs/Application.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @SpringBootApplication
 @MapperScan("org.geekbang.projects.cs.mapper")
+@EnableFeignClients(basePackages = {"org.geekbang.projects.cs.provider"})
 public class Application {
 
     @Bean
